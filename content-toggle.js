ea860c6623b60b58a85845c22904f6cb622a5651
MinecraftZapolator <annmarie.simon.arthur@gmail.com>
2022-10-21 12:31:50 -0500

    Add files via upload

---
+++ content-toggle.js
@@ -0,0 +1,5 @@
+function toggleShow(id) {
+    if (document.getElementById(id).style.display == 'block') {
+        document.getElementById(id).style.display = 'none'; }
+    else { document.getElementById(id).style.display = 'block'; }
+  }
